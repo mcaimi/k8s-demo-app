2a8ceff181666fa7876679b8c2ffc1ce4341c1c5
mcaimi <caimi.marco@protonmail.ch>
2020-03-30 17:39:35 +0200

    demoapp: added flyway migrations for the preprod environment

---
+++ src/main/resources/db/migrations/preprod/V1.1.0__notes.sql
@@ -0,0 +1,9 @@
+INSERT INTO notes(id, contents, name) VALUES (999, 'i am some content', 'test note 1');
+INSERT INTO notes(id, contents, name) VALUES (1000, 'yup, contents galore','test note 2');
+INSERT INTO notes(id, contents, name) VALUES (1001, 'hey look here!', 'test note 3');
+INSERT INTO notes(id, contents, name) VALUES (1002, 'buy some books plz','test note 4');
+INSERT INTO notes(id, contents, name) VALUES (1003, 'i am a test note','test note 5');
+INSERT INTO notes(id, contents, name) VALUES (1004, 'filling some more rows','test note 6');
+INSERT INTO notes(id, contents, name) VALUES (1005, 'useless data','test note 7');
+INSERT INTO notes(id, contents, name) VALUES (1006, 'even less useful data','test note 8');
+INSERT INTO notes(id, contents, name) VALUES (1007, 'do not mind me','test note 9');
