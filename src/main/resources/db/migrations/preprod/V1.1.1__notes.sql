e76e70808eea566d96d9b810e6f6d523886606f4
mcaimi <caimi.marco@protonmail.ch>
2020-04-08 12:31:54 +0200

    demoapp: added some more entries in the database migration scripts

---
+++ src/main/resources/db/migrations/preprod/V1.1.1__notes.sql
@@ -0,0 +1,4 @@
+INSERT INTO notes(id, contents, name) VALUES (235, 'some more content', 'preprod note 7');
+INSERT INTO notes(id, contents, name) VALUES (456, 'I really should be deleted from web console','preprod delete_me');
+INSERT INTO notes(id, contents, name) VALUES (6787, 'again, I am useless', 'preprod note 77');
+INSERT INTO notes(id, contents, name) VALUES (1111, 'last pre-prod note','preprod 4');
---
+++ src/main/resources/db/migrations/test/V1.1.1__notes.sql
@@ -0,0 +1,4 @@
+INSERT INTO notes(id, contents, name) VALUES (235, 'some more content', 'test note 7');
+INSERT INTO notes(id, contents, name) VALUES (456, 'I really should be deleted from web console','delete_me');
+INSERT INTO notes(id, contents, name) VALUES (6787, 'again, I am useless', 'test note 77');
+INSERT INTO notes(id, contents, name) VALUES (1111, 'last test note','test 4');
